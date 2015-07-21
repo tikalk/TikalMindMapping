844bc2c2fb69f40d9df2e1045bb5d33e83af4ad0
shaiy <s.yerushalmi@initialrewards.com>
2015-07-21 15:06:08 +0300

    graphFactory +  mock daTA

--- client/app/graph/graphFactory.js
+++ client/app/graph/graphFactory.js
@@ -1,3 +1,12 @@
 /**
  * Created by shaiyerushalmi on 7/21/15.
  */
+angular.module('mindmapApp').factory('graphFactory',[function(){
+    var graphData = mindMapVars.mockGraphData;
+
+    return {
+        graphData: graphData
+    }
+
+
+}]);
