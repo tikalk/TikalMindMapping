0fa5ceaabe4a56cd7dcc1d12c6bd204d957e8860
ereztikal <erez@tikalk.com>
2015-07-21 12:04:05 +0300

    added employee edit view first implementation

--- client/app/mindmap.module.js
+++ client/app/mindmap.module.js
@@ -1,8 +1,19 @@
 /**
  * Created by robertferentz on 21/07/15.
  */
-(function(angular){
-    angular.module('mindmap',['ngSanitize']).controller('main',['$scope',function($scope){
-        $scope.appName = 'Tikal Mind Mapping';
-    }]);
-})(angular);
+var mindmapApp = angular.module('mindmapApp', ['ngSanitize', 'ui.router']);
+
+mindmapApp.controller('main', ['$scope',function($scope) {
+    $scope.appName = 'Tikal Mind Mapping';
+}]);
+
+mindmapApp.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/employee");
+
+    $stateProvider
+        .state('employee', {
+            url: "/employee",
+            templateUrl: "app/employee/editEmployee.tpl.html",
+            controller: ''
+        });
+});
