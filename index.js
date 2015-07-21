973fe0cb1f164ec891e87b37b93b62c31ca6996d
jsblackbelt <sefi@tikalk.com>
2015-07-21 14:45:49 +0300

    add user controller and express mappings

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 /**
  * Created by robertferentz on 21/07/15.
  */
+'use strict';
+
 var express = require('express');
 var app = express();
 
+var users = require('./server/Users');
 
 app.use(express.static('client'));
 app.use('/libs',express.static('bower_components'));
+
+var router  = express.router();
+router.use('/user', users.controller);
+app.use('/backend', router);
+
 var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
     console.log('Tikal mind mapping listening at http://%s:%s', host, port);
 });
+
+module.exports = app;
